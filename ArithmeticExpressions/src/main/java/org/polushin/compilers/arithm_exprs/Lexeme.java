1d9e52319fe270d16a852f8ea5edcb9d28d4e121
Kirill Polushin <kirill@polushin.org>
2019-02-06 19:52:08 +0700

    Refactor lexeme and lexeme type

--- ArithmeticExpressions/src/main/java/org/polushin/compilers/arithm_exprs/Lexeme.java
+++ ArithmeticExpressions/src/main/java/org/polushin/compilers/arithm_exprs/Lexeme.java
@@ -2,15 +2,6 @@ package org.polushin.compilers.arithm_exprs;
 
 public class Lexeme {
 
-    public static final Lexeme PLUS = new Lexeme(Type.PLUS, "+");
-    public static final Lexeme MINUS = new Lexeme(Type.MINUS, "-");
-    public static final Lexeme MULTIPLICATION = new Lexeme(Type.MULTIPLICATION, "*");
-    public static final Lexeme DIVISION = new Lexeme(Type.DIVISION, "/");
-    public static final Lexeme POWER = new Lexeme(Type.POWER, "^");
-    public static final Lexeme LEFT_BRACKET = new Lexeme(Type.LEFT_BRACKET, "(");
-    public static final Lexeme RIGHT_BRACKET = new Lexeme(Type.RIGHT_BRACKET, ")");
-    public static final Lexeme EOF = new Lexeme(Type.EOF, null);
-
     public final Type type;
     public final String value;
 
@@ -19,15 +10,29 @@ public class Lexeme {
         this.value = value;
     }
 
+    public Lexeme(Type type) {
+        this(type, type.defaultValue);
+    }
+
     public enum Type {
         NUMBER,
-        PLUS,
-        MINUS,
-        MULTIPLICATION,
-        DIVISION,
-        POWER,
-        LEFT_BRACKET,
-        RIGHT_BRACKET,
-        EOF
+        PLUS("+"),
+        MINUS("-"),
+        MULTIPLICATION("*"),
+        DIVISION("/"),
+        POWER("^"),
+        LEFT_BRACKET("("),
+        RIGHT_BRACKET(")"),
+        EOF(null);
+
+        private final String defaultValue;
+
+        Type(String defaultValue) {
+            this.defaultValue = defaultValue;
+        }
+
+        Type() {
+            this(null);
+        }
     }
 }
