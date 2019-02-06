193408c924e42dcf70b7de1949b984d451206df6
Kirill Polushin <kirill@polushin.org>
2019-02-06 19:48:24 +0700

    Add simple lexemes constants

--- ArithmeticExpressions/src/main/java/org/polushin/compilers/arithm_exprs/Lexeme.java
+++ ArithmeticExpressions/src/main/java/org/polushin/compilers/arithm_exprs/Lexeme.java
@@ -2,6 +2,15 @@ package org.polushin.compilers.arithm_exprs;
 
 public class Lexeme {
 
+    public static final Lexeme PLUS = new Lexeme(Type.PLUS, "+");
+    public static final Lexeme MINUS = new Lexeme(Type.MINUS, "-");
+    public static final Lexeme MULTIPLICATION = new Lexeme(Type.MULTIPLICATION, "*");
+    public static final Lexeme DIVISION = new Lexeme(Type.DIVISION, "/");
+    public static final Lexeme POWER = new Lexeme(Type.POWER, "^");
+    public static final Lexeme LEFT_BRACKET = new Lexeme(Type.LEFT_BRACKET, "(");
+    public static final Lexeme RIGHT_BRACKET = new Lexeme(Type.RIGHT_BRACKET, ")");
+    public static final Lexeme EOF = new Lexeme(Type.EOF, null);
+
     public final Type type;
     public final String value;
 
