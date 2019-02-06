98453528ef126b5bc2e996b34afbbabd7563f3e5
Kirill Polushin <kirill@polushin.org>
2019-02-06 21:55:14 +0700

    Create parser implementation

--- ArithmeticExpressions/src/main/java/org/polushin/compilers/arithm_exprs/Parser.java
+++ ArithmeticExpressions/src/main/java/org/polushin/compilers/arithm_exprs/Parser.java
@@ -3,6 +3,8 @@ package org.polushin.compilers.arithm_exprs;
 import java.io.IOException;
 import java.text.ParseException;
 
+import static org.polushin.compilers.arithm_exprs.Lexeme.Type.*;
+
 public class Parser {
 
     private final Lexer lexer;
@@ -14,27 +16,75 @@ public class Parser {
     }
 
     public int executeCalculations() throws IOException, ParseException {
-        return 0;
+        int result = executeExpr();
+        if (curr.type != EOF)
+            throw new ParseException("Expected EOF", 0);
+        return result;
     }
 
     private int executeExpr() throws IOException, ParseException {
-        return 0;
+        int result = executeTerm();
+        Lexeme.Type type = curr.type;
+
+        while (type == PLUS || type == MINUS) {
+            curr = lexer.nextLexeme();
+            if (type == PLUS)
+                result += executeTerm();
+            else
+                result -= executeTerm();
+            type = curr.type;
+        }
+
+        return result;
     }
 
     private int executeTerm() throws IOException, ParseException {
-        return 0;
+        int result = executeFractal();
+        Lexeme.Type type = curr.type;
+
+        while (type == MULTIPLICATION || type == DIVISION) {
+            curr = lexer.nextLexeme();
+            if (type == MULTIPLICATION)
+                result *= executeFractal();
+            else
+                result /= executeFractal();
+            type = curr.type;
+        }
+
+        return result;
     }
 
     private int executeFractal() throws IOException, ParseException {
-        return 0;
+        int result = executePower();
+        if (curr.type == POWER) {
+            curr = lexer.nextLexeme();
+            return (int) Math.pow(result, executeFractal());
+        }
+        return result;
     }
 
     private int executePower() throws IOException, ParseException {
-        return 0;
+        if (curr.type == MINUS) {
+            curr = lexer.nextLexeme();
+            return -executeAtom();
+        }
+        return executeAtom();
     }
 
     private int executeAtom() throws IOException, ParseException {
-        return 0;
+        if (curr.type == NUMBER) {
+            int result = Integer.parseInt(curr.value);
+            curr = lexer.nextLexeme();
+            return result;
+        } else if (curr.type == LEFT_BRACKET) {
+            curr = lexer.nextLexeme();
+            int result = executeExpr();
+            if (curr.type != RIGHT_BRACKET)
+                throw new ParseException("Expected close bracket", 0);
+            curr = lexer.nextLexeme();
+            return result;
+        } else
+            throw new ParseException("Unexpected lexeme type: " + curr.type.name(), 0);
     }
 
 }
