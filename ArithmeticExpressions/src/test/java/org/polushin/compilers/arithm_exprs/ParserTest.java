3f5d2f7bd7d7ac6a4c7dc02eb71a95113c5bdb87
Kirill Polushin <kirill@polushin.org>
2019-02-07 12:31:53 +0700

    Improve parser tests

--- ArithmeticExpressions/src/test/java/org/polushin/compilers/arithm_exprs/ParserTest.java
+++ ArithmeticExpressions/src/test/java/org/polushin/compilers/arithm_exprs/ParserTest.java
@@ -18,24 +18,30 @@ class ParserTest {
     }
 
     @Test
-    void executeCalculationsRightSimple() throws IOException, ParseException {
-        assertCalcs("0", 0);
-        assertCalcs("5", 5);
+    void executeCalculationsSingleNumber() throws IOException, ParseException {
         assertCalcs("12300", 12300);
+    }
 
+    @Test
+    void executeCalculationsSingleOperation() throws IOException, ParseException {
         assertCalcs("2 + 2", 4);
         assertCalcs("10 - 28", -18);
         assertCalcs("25 * 4", 100);
         assertCalcs("500 / 25", 20);
         assertCalcs("2 ^ 12", 4096);
+    }
 
+    @Test
+    void executeCalculationsBracketsSingleNumber() throws IOException, ParseException {
         assertCalcs("(15)", 15);
         assertCalcs("((128))", 128);
         assertCalcs("((((2048))))", 2048);
+    }
 
+    @Test
+    void executeCalculationsBracketsAndOperations() throws IOException, ParseException {
         assertCalcs("(- 9)", -9);
         assertCalcs("100 + (-100)", 0);
-
         assertCalcs("(2 + 2)", 4);
         assertCalcs("((10 - 28))", -18);
         assertCalcs("(25) * (4)", 100);
@@ -51,19 +57,30 @@ class ParserTest {
     }
 
     @Test
-    void executeCalculationsWrong() {
+    void executeCalculationsWrongSyntaxNoOperands() {
         assertThrows(ParseException.class, () -> assertCalcs("", 0));
         assertThrows(ParseException.class, () -> assertCalcs("25 42", 0));
         assertThrows(ParseException.class, () -> assertCalcs("+ +", 0));
         assertThrows(ParseException.class, () -> assertCalcs("14 + 28 * 3 -", 0));
+    }
+
+    @Test
+    void executeCalculationsWrongSyntaxBrackets() {
         assertThrows(ParseException.class, () -> assertCalcs("(", 0));
         assertThrows(ParseException.class, () -> assertCalcs(")", 0));
         assertThrows(ParseException.class, () -> assertCalcs("()", 0));
         assertThrows(ParseException.class, () -> assertCalcs("(56 + 44", 0));
+    }
+
+    @Test
+    void executeCalculationsWrongSyntaxBracketsAndNoOperants() {
         assertThrows(ParseException.class, () -> assertCalcs("24 * (2-)", 0));
         assertThrows(ParseException.class, () -> assertCalcs("56 + (*1)", 0));
         assertThrows(ParseException.class, () -> assertCalcs("(56 + 44) 32 - 2", 0));
+    }
 
+    @Test
+    void executeCalculationsDivisionByZero() {
         assertThrows(ArithmeticException.class, () -> assertCalcs("5 / 0", 0));
         assertThrows(ArithmeticException.class, () -> assertCalcs("5 / (20 - 20)", 0));
         assertThrows(ArithmeticException.class, () -> assertCalcs("5 / (1 / 100)", 0));
